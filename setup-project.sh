02cfa9d233dfb239de496e6ced6fa8c521143ab4
Lars Nielsen <lnc13.lars@gmail.com>
2019-04-11 08:09:03 +0200

    update path

--- setup-project.sh
+++ setup-project.sh
@@ -6,10 +6,10 @@ waf_au_extensions="waf_au_extensions"
 
 raw_project_essentials="https://raw.githubusercontent.com/AgileCloudLab/project-stabels/master/"
 
-doxyfile="Doxyfile?token=AB2n8H__01I12fePHS52ZeL9DraRAOXgks5ckNa_wA%3D%3D"
-waffile="waf?token=AB2n8AskKBSzGugYn0Mqj3witaJ3R1f0ks5ckNhWwA%3D%3D"
-wscriptfile="wscript?token=AB2n8LjPhubXD0ZV7q-wgbFIHEay5ejgks5ckNi3wA%3D%3D"
-resolvefile="resolve.json?token=AB2n8Nmgcapx_k1wn9FoieL9zq3fbaBGks5ckNigwA%3D%3D"
+doxyfile="Doxyfile"
+waffile="waf"
+wscriptfile="wscript"
+resolvefile="resolve.json"
 
 git clone "$base_git$project_name.git" &&
     cd $project_name &&
